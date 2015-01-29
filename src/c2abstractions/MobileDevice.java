f46a733e627d408f91014c3439543c91776c9b61
jseib <jseib@my.wctc.edu>
2015-01-28 21:43:43 -0600

    Chapter 2 Abstractions Class/Interface

---
+++ src/c2abstractions/MobileDevice.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package c2abstractions;
+
+/**
+ *
+ * @author Jon
+ */
+public class MobileDevice implements Device {
+    
+}
